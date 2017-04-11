b8a5764fd45c778992d85139e3e6781515418e54
Kaida Masaki <kaida.masaki@socrata.com>
2017-04-11 14:31:33 -0700

    test.sh improvements

--- test.sh
+++ test.sh
@@ -3,7 +3,21 @@
 # Simple thing to watch your project and run tests on change
 # Takes one optional argument, which will grep tests and run the ones that match
 
-while inotifywait -r -e modify ./lib ./test; do
-  echo "Running unit tests"
-  mix test
-done
+if [ -z "${SOCRATA_USER}" ] || [ -z "${SOCRATA_PASSWORD}" ]; then
+    echo "\$SOCRATA_USER and \$SOCRATA_PASSWORD are required!"
+    echo "Please set them in your environment!"
+    exit 1
+fi
+
+echo "Installing dependencies"
+mix deps.get || exit "$1"
+
+if [ -x "$(which inotifywait)" ]; then
+    while inotifywait -r -e modify ./lib ./test; do
+        echo "Running unit tests"
+        mix test
+    done
+else
+    echo "Running unit tests"
+    mix test
+fi
