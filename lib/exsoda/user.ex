169bf9a268739d8df5e90e0371e6766f52d9a494
Quigley Malcolm <quigley.malcolm@tylertech.com>
2018-12-28 15:13:51 -0800

    EN-27164 user module can get by id now

--- lib/exsoda/user.ex
+++ lib/exsoda/user.ex
@@ -5,6 +5,13 @@ defmodule Exsoda.User do
     defstruct opts: []
   end
 
+  defmodule ById do
+    defstruct id: "", opts: []
+  end
+
   def current(opts \\ []), do: %Current{opts: Http.options(opts)}
+  def by_id(id, opts \\ []), do: %ById{id: id, opts: Http.options(opts)}
+
   def run(%Current{} = q), do: Http.get("/users/current", q)
+  def run(%ById{id: id} = q), do: Http.get("/users/#{Http.encode(id)}", q)
 end
