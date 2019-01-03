4dcef2f3e111649df1fef7a24c0cc6edb808912a
Quigley Malcolm <quigley.malcolm@tylertech.com>
2019-01-02 17:01:55 -0800

    can get view by fourfour

--- lib/exsoda/view.ex
+++ lib/exsoda/view.ex
@@ -1,4 +1,13 @@
 defmodule Exsoda.View do
+  alias Exsoda.Http
   defstruct fourfour: nil,
     name: nil
+
+  defmodule ByFourFour do
+    defstruct fourfour: "", opts: []
+  end
+
+  def by_fourfour(fourfour, opts \\ []), do: %ByFourFour{fourfour: fourfour, opts: Http.options(opts)}
+
+  def run(%ByFourFour{fourfour: fourfour} = q), do: Http.get("/views/#{Http.encode(fourfour)}", q)
 end
