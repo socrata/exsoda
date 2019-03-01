0211299c64608b23a9c7a9115848459b25dd98ab
rozap <chrisd1891@gmail.com>
2019-02-28 16:44:37 -0800

    add api key delete

--- lib/exsoda/apikeys.ex
+++ lib/exsoda/apikeys.ex
@@ -16,8 +16,19 @@ defmodule Exsoda.ApiKeys do
     end
   end
 
+  defmodule Delete do
+    @enforce_keys [:keyId]
+    defstruct [:keyId]
+
+    defimpl Execute, for: __MODULE__ do
+      def run(%Delete{} = d, o) do
+        Http.delete("/api_keys/#{d.keyId}", o)
+      end
+    end
+  end
 
   def new(options \\ []), do: Runner.new(options)
   def run(operations), do: Runner.run(operations)
   def create(%Operations{} = o, name), do: prepend(%Create{keyName: name}, o)
+  def delete(%Operations{} = o, id), do: prepend(%Delete{keyId: id}, o)
 end
