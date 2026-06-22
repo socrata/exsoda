7321e7da3c03d10f58674f039f37a92d7e23ab6d
Sam Reep <sam.reep@tylertech>
2026-06-22 15:27:19 -0700

    update elixir version in project description

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exsoda.Mixfile do
   def project do
     [app: :exsoda,
      version: "4.2.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.17.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
